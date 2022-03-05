1412f75a97ff821fe92e128e9ff746259485e202
Jack <jack.house@linuxmail.org>
2022-03-05 11:20:23 +0100

    Collegamento DB per tabella review

---
+++ server/databaseModule/src/it/prova/ClasseMain.java
@@ -0,0 +1,21 @@
+package it.prova;
+
+import java.util.List;
+
+public class ClasseMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ReviewDatabase rdb = new ReviewDatabase ();
+		List <Review> Lista = rdb.getReview();
+		System.out.println (Lista);
+	Lista.forEach(System.out::println);
+	System.out.println ("\n \n \n");
+	
+	Review r = rdb.getFromId(5);
+	System.out.println (r);
+
+	}
+
+}
