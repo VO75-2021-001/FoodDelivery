3e99f1fad59f5c850ca3d0c8fee12d2716c0efa9
unknown <giovacecio@gmail.com>
2022-03-12 11:26:35 +0100

    Insert Update Delete Tabella FornitoriServizi

--- src/server/databaseModule/food/delivery/database/FornitoriServizi.java
+++ src/server/databaseModule/food/delivery/database/FornitoriServizi.java
@@ -62,13 +62,346 @@ public class FornitoriServizi {
             //mando in return l'oggetto letto
             return tabFornitoriServizi;
         } catch (ClassNotFoundException e) {
-            System.out.println(" e: " +  e);
+            System.out.println("ClassNotFoundException e: " +  e);
             //mando in return la lista con tutti gli oggetti
             return null;
         } catch (SQLException e) {
-            System.out.println(" e: " +  e);
+            System.out.println("SQLException e: " +  e);
             //mando in return la lista con tutti gli oggetti
             return null;
         }
     }
+    public boolean insertfornitoriservizi(int infornitore,int inservizio){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "INSERT INTO fornitoriservizi (IDFornitore,IDServizio) VALUES IDFornitore = ? and IDServizio = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, infornitore);
+            preparedStatement.setInt(2, inservizio);
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+        
+    }
+
+    public boolean updateIds(int upidfornitore,int upidservizio,int pk,int spk){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDFornitore = ? AND IDServizio =? WHERE IDFornitore = ? and IDServizio = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidfornitore);
+            preparedStatement.setInt(2, upidservizio);
+            preparedStatement.setInt(3, pk);
+            preparedStatement.setInt(4, spk);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+        
+    }
+    public boolean updateIds(int upidfornitore,int upidservizio){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDFornitore = ? AND IDServizio =?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidfornitore);
+            preparedStatement.setInt(2, upidservizio);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+        
+    }
+    public boolean updateIdFrn(int upidfornitore){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDFornitore = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidfornitore);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+    }
+    public boolean updateIdFrn(int upidfornitore,int idfornitore){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDFornitore = ? WHERE IDFornitore = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidfornitore);
+            preparedStatement.setInt(2, idfornitore);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+    }
+    public boolean updateIdFrn(int upidfornitore,int idfornitore,int idservizio){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDFornitore = ? WHERE IDFornitore = ? and IDServizio = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidfornitore);
+            preparedStatement.setInt(2, idfornitore);
+            preparedStatement.setInt(3, idservizio);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+    }
+    public boolean updateIdSrv(int upidservizio){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDServizio = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidservizio);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+        
+    }
+    public boolean updateIdSrv(int upidservizio,int idservizio){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDServizio = ? WHERE IDFornitore = ? and IDServizio = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidservizio);
+            preparedStatement.setInt(2, idservizio);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+    }
+    public boolean updateIdSrv(int upidservizio,int idfornitore,int idservizio){
+        int result = 0;
+        try {
+             //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "UPDATE fornitoriservizi SET IDServizio = ? WHERE IDFornitore = ? and IDServizio = ? ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, upidservizio);
+            preparedStatement.setInt(2, idfornitore);
+            preparedStatement.setInt(3, idservizio);
+
+            result = preparedStatement.executeUpdate();
+
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+    }
+    public boolean deleteFromId(int pk,int spk) {
+        int result = 0;
+        try {
+            //carichiamo il driver
+           Class.forName("com.mysql.jdbc.Driver");
+           //Creiamo l'oggetto di connesione (cambiare i dati in maiuscolo con i vostri)
+           Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fooddelivery", "servizio", "servizio");
+            //genero la stringa di query
+            String query = "DELETE FROM fornitoriservizi WHERE IDFornitore = ? and IDServizio = ?";
+            //creo il preparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            //imposto i valori dei vari ? presenti nella query (1 in questo caso)
+            preparedStatement.setInt(1, pk);
+            preparedStatement.setInt(2, spk);
+            result = preparedStatement.executeUpdate();
+            if(result > 0){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        } catch (SQLException e) {
+            System.out.println("SQLException e: " +  e);
+            //mando in return la lista con tutti gli oggetti
+            return false;
+        }
+    }
+
 }
